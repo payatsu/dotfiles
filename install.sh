d5d3ef996bfd87db6b9b7377eda8ff6cdd3679e5
payatsu <6873447+payatsu@users.noreply.github.com>
2016-09-21 22:30:32 +0900

    install.shの改良。

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 dir=`dirname \`readlink -f $0\``
 cmd=`basename $0`
-find ${dir} -mindepth 1 -maxdepth 1 ! -name ${cmd} ! -name '*.swp' -exec cp -t ${HOME} -ruv {} +
+LANG=C find ${dir} -mindepth 1 -maxdepth 1 ! -name ${cmd} ! -name '*.swp' -exec cp -t ${HOME} -iruv {} +
