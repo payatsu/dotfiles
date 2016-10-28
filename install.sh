8a4451590be563d27c044e7c4fda30e24a54abb6
payatsu <6873447+payatsu@users.noreply.github.com>
2016-10-28 22:32:28 +0900

    aliasを使って、vimがvim --servernameになるようにした。

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 dir=`dirname \`readlink -f $0\``
 cmd=`basename $0`
-LANG=C find ${dir} -mindepth 1 -maxdepth 1 ! -name ${cmd} ! -name '*.swp' -exec cp -t ${HOME} -iruv {} +
+LANG=C find ${dir} -mindepth 1 -maxdepth 1 \( -name ${cmd} -o -name '.*.swp' \) -prune -o -exec cp -t ${HOME} -iruv {} +
