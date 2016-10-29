5dc98728b28ef96947dbb56ec5bb795c36cbca07
payatsu <6873447+payatsu@users.noreply.github.com>
2016-10-29 15:23:10 +0900

    install.shの修正に誤りがあったので直した。

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 dir=`dirname \`readlink -f $0\``
 cmd=`basename $0`
 LANG=C find ${dir} -mindepth 1 -type d | sed -e "s%^${dir}%${HOME}%" | xargs mkdir -p
-LANG=C find ${dir} -mindepth 1 \( -name ${cmd} -o -name '*.swp' \) -prune -o -type f -print | sed -e "s%^${dir}\([[:graph:]]\+\)\$%cp -iuv & ${HOME}\1%" | sh
+LANG=C find ${dir} -mindepth 1 \( -name ${cmd} -o -name '*.swp' \) -prune -o -type f -exec sh -c 'cp -iuv {} `echo {} | sed -e s%^'${dir}'%${HOME}%`' \;
