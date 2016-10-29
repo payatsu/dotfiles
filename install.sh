2792f43ebc8d88e40e0bc93c0756c6e649ba581a
payatsu <6873447+payatsu@users.noreply.github.com>
2016-10-29 13:18:06 +0900

    install.shのアルゴリズム変更(cp -r不使用にした)。

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 dir=`dirname \`readlink -f $0\``
 cmd=`basename $0`
-LANG=C find ${dir} -mindepth 1 -maxdepth 1 \( -name ${cmd} -o -name '.*.swp' \) -prune -o -exec cp -t ${HOME} -iruv {} +
+LANG=C find ${dir} -mindepth 1 -type d | sed -e "s%^${dir}%${HOME}%" | xargs mkdir -p
+LANG=C find ${dir} -mindepth 1 \( -name ${cmd} -o -name '*.swp' \) -prune -o -type f -print | sed -e "s%^${dir}\([[:graph:]]\+\)\$%cp -iuv & ${HOME}\1%" | sh
