d586c6da211e891dcc0cfab867781b19016fc7b6
payatsu <6873447+payatsu@users.noreply.github.com>
2017-01-18 22:23:08 +0900

    .screenrcから.screen/screenrc.localを読むようにした。

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 dir=`dirname \`readlink -f $0\``
 cmd=`basename $0`
-LANG=C find ${dir} -mindepth 1 -type d | sed -e "s%^${dir}%${HOME}%" | xargs mkdir -p
+LANG=C find ${dir} -mindepth 1 -type d | sed -e "s%^${dir}%${HOME}%" | xargs mkdir -pv
 LANG=C find ${dir} -mindepth 1 \( -name ${cmd} -o -name '*.swp' \) -o -empty -prune -o -type f -exec sh -c 'cp -iuv {} `echo {} | sed -e s%^'${dir}'%${HOME}%`' \;
