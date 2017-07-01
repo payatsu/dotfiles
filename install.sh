27613852f91f3d6b1da927b269971ff360bca1aa
payatsu <6873447+payatsu@users.noreply.github.com>
2017-07-01 18:29:19 +0900

    install.shに -y オプション追加。

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/sh -e
 
+yes=
+while getopts y arg; do
+	case ${arg} in
+	y) yes='yes | ';;
+	esac
+done
+shift `expr ${OPTIND} - 1`
+
 dir=`dirname \`readlink -f $0\``
 cmd=`basename $0`
 LANG=C find ${dir} -mindepth 1 -type d | sed -e "s%^${dir}%${HOME}%" | xargs mkdir -pv
-LANG=C find ${dir} -mindepth 1 \( -name ${cmd} -o -name '*.swp' \) -o -empty -prune -o -type f -exec sh -c 'cp -iuv {} `echo {} | sed -e s%^'${dir}'%${HOME}%`' \;
+LANG=C find ${dir} -mindepth 1 \( -name ${cmd} -o -name '*.swp' \) -o -empty -prune -o -type f -exec sh -c "${yes}"'cp -iuv {} `echo {} | sed -e s%^'${dir}'%${HOME}%`' \;
