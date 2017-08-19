1295d41c0cd6e79b49c907a7ac2eebbaa5a62a9d
payatsu <6873447+payatsu@users.noreply.github.com>
2017-08-20 03:36:47 +0900

    dotfiles/install.shで-sによるシンボリック形式のインストールに対応。

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -e
 
+s=
 yes=
-while getopts y arg; do
+while getopts sy arg; do
 	case ${arg} in
+	s) s=s;;
 	y) yes='yes | ';;
 	esac
 done
@@ -11,4 +13,4 @@ shift `expr ${OPTIND} - 1`
 dir=`dirname \`readlink -f $0\``
 cmd=`basename $0`
 LANG=C find ${dir} -mindepth 1 -type d | sed -e "s%^${dir}%${HOME}%" | xargs mkdir -pv
-LANG=C find ${dir} -mindepth 1 \( -name ${cmd} -o -name '*.swp' \) -o -empty -prune -o -type f -exec sh -c "${yes}"'cp -iuv {} `echo {} | sed -e s%^'${dir}'%${HOME}%`' \;
+LANG=C find ${dir} -mindepth 1 \( -name ${cmd} -o -name '*.swp' \) -o -empty -prune -o -type f -exec sh -c "${yes}"'cp -i'${s}'uv {} `echo {} | sed -e s%^'${dir}'%${HOME}%`' \;
