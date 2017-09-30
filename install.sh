7eaa612293b5d493371aa99b7c2d7838b5558f68
payatsu <6873447+payatsu@users.noreply.github.com>
2017-09-30 09:52:08 +0900

    vim-clangのclang_{c,cpp}_completeoptを変えた。

--- install.sh
+++ install.sh
@@ -13,4 +13,4 @@ shift `expr ${OPTIND} - 1`
 dir=`dirname \`readlink -f $0\``
 cmd=`basename $0`
 LANG=C find ${dir} -mindepth 1 -type d | sed -e "s%^${dir}%${HOME}%" | xargs mkdir -pv
-LANG=C find ${dir} -mindepth 1 \( -name ${cmd} -o -name '*.swp' \) -o -empty -prune -o -type f -exec sh -c "${yes}"'cp -i'${s}'uv {} `echo {} | sed -e s%^'${dir}'%${HOME}%`' \;
+LANG=C find ${dir} -mindepth 1 \( -name ${cmd} -o -name '*.swp' -o -name seq.puml -o -name '*.png' \) -o -empty -prune -o -type f -exec sh -c "${yes}"'cp -i'${s}'uv {} `echo {} | sed -e s%^'${dir}'%${HOME}%`' \;
