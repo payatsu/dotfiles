a95eac95a8fd3d725bdbf50e34bccd6dd41785f4
payatsu <6873447+payatsu@users.noreply.github.com>
2020-04-24 17:04:52 +0900

    Create LICENSE

--- install.sh
+++ install.sh
@@ -12,5 +12,5 @@ shift `expr ${OPTIND} - 1`
 
 dir=`dirname \`readlink -f $0\``
 cmd=`basename $0`
-LANG=C find ${dir} -mindepth 1 -type d | sed -e "s%^${dir}%${HOME}%" | xargs mkdir -pv
-LANG=C find ${dir} -mindepth 1 \( -name ${cmd} -o -name '*.swp' -o -name seq.puml -o -name '*.png' \) -o -empty -prune -o -type f -exec sh -c "${yes}"'cp -i'${s}'uv {} `echo {} | sed -e s%^'${dir}'%${HOME}%`' \;
+LANG=C find ${dir} -mindepth 1 -name .git -prune -o -type d -print | sed -e "s%^${dir}%${HOME}%" | xargs mkdir -pv
+LANG=C find ${dir} -mindepth 1 \( -name ${cmd} -o -name .git -o -name '*.swp' -o -name seq.puml -o -name '*.png' -o -name LICENSE \) -prune -o -empty -prune -o -type f -exec sh -c "${yes}"'cp -i'${s}'uv {} `echo {} | sed -e s%^'${dir}'%${HOME}%`' \;
